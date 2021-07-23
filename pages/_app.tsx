ab85d0b0d3afd2a00141e14a928507a0e6732982
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-07-24 01:28:05 +0800

    Add `enableSystem` on ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '@/styles/global.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" enableSystem={false}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
