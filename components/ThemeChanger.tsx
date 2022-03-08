04967513c57a0036ca0dee371a56ffcf3673cddf
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-03-09 00:50:30 +0800

    Replace theme changer button with switch

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
 
 import { useTheme } from 'next-themes';
-import { MoonIcon, SunIcon } from '@heroicons/react/outline';
-
-import Button from '@/components/Button';
+import { MoonIcon, SunIcon } from '@heroicons/react/solid';
+import { Switch } from '@headlessui/react';
 
 const ThemeChanger = () => {
   const [mounted, setMounted] = React.useState(false);
   const { theme, setTheme } = useTheme();
 
+  const enabled = theme === 'dark';
+
   React.useEffect(() => setMounted(true), []);
 
-  if (!mounted) return null;
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <Button
-      outlined={true}
-      narrow={true}
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    <Switch
+      checked={enabled}
+      onChange={() => setTheme(enabled ? 'light' : 'dark')}
+      className={`
+        ${enabled ? 'bg-purple-500' : 'bg-purple-500'}
+        relative inline-flex items-center h-6 rounded-full w-11 relative
+      `}
     >
-      {theme === 'dark' ? (
-        <SunIcon className="w-4 h-4 inline-block" />
+      <span className="sr-only">Enable dark mode</span>
+      {enabled ? (
+        <MoonIcon className={`w-4 h-4 text-white absolute left-1 top-1`} />
       ) : (
-        <MoonIcon className="w-4 h-4 inline-block" />
+        <SunIcon className={`w-4 h-4 text-white absolute right-1 top-1`} />
       )}
-      <span className="align-middle text-sm ml-1 lg:hidden">
-        Switch to {theme === 'dark' ? 'light' : 'dark'} theme
-      </span>
-    </Button>
+      <span
+        className={`${
+          enabled ? 'translate-x-6' : 'translate-x-1'
+        } inline-block w-4 h-4 transform bg-white rounded-full transition ease-in-out duration-200`}
+      />
+    </Switch>
   );
 };
 
