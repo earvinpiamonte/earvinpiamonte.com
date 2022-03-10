fe96774d5be7335730f5b36d08c8b8defb901df3
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-03-11 00:10:50 +0800

    Move Uses to My Gear page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -89,6 +89,7 @@ const Nav = () => {
             <NavItem title="Portfolio" path="/portfolio" />
             <NavItem title="Crafts" path="/crafts" />
             <NavItem title="Blog" path="/blog" />
+            <NavItem title="My Gear" path="/my-gear" />
           </div>
           <div>
             <ThemeChanger />
