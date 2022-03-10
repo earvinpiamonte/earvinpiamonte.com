74c4e802e9e2ee17dffb96fe4e97cb4ccc0e4cc1
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-03-10 23:01:00 +0800

    Update calendar size; Update spacing

--- components/BlogPostItem.tsx
+++ components/BlogPostItem.tsx
@@ -13,11 +13,11 @@ const BlogPostItem = ({ ...post }: PostType) => {
 
   return (
     <div className="border dark:border-gray-600 flex mb-16 rounded-lg">
-      <div className="border-r dark:border-gray-600 flex-none rounded-l-lg w-32">
+      <div className="border-r dark:border-gray-600 flex-none rounded-l-lg w-28">
         <div className="bg-purple-500 border-b border-purple-500 font-medium px-1 py-1 rounded-tl-lg text-center text-gray-200 uppercase">
           {new Date(date).toLocaleString('en-US', { month: 'short' })}
         </div>
-        <div className="bg-gray-100 dark:bg-black py-4 text-center">
+        <div className="bg-gray-100 dark:bg-black py-2 text-center">
           <h3 className="dark:text-gray-400 font-medium mb-0 text-4xl text-gray-800">
             {new Date(date).toLocaleString('en-US', { day: '2-digit' })}
           </h3>
@@ -28,7 +28,7 @@ const BlogPostItem = ({ ...post }: PostType) => {
           </p>
         </div>
       </div>
-      <div className="overflow-hidden px-4 py-5">
+      <div className="overflow-hidden px-4 py-3">
         <h2 className="mb-1 text-xl truncate">
           <Link href={`/${type}/${slug}`}>
             <a className="dark:hover:text-purple-500 dark:text-gray-400 font-medium hover:text-purple-500 hover:underline text-gray-800">
