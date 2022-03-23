c70437ac824f43eb0af989495609d926bd2510b0
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-03-24 03:35:21 +0800

    Add footer links; Update `CustomLink` component

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -1,20 +1,20 @@
 import Link from 'next/link';
 
 const CustomLink = (props) => {
-  const { href } = props;
+  const { href, className } = props;
   const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <a className="app-link" {...props} />
+        <a className={className || `app-link`} {...props} />
       </Link>
     );
   }
 
   return (
     <a
-      className="app-link"
+      className={className || `app-link`}
       target="_blank"
       rel="noopener noreferrer"
       {...props}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ import getConfig from 'next/config';
 
 import Container from '@/components/Container';
 import NowPlaying from '@/components/NowPlaying';
+import CustomLink from '@/components/CustomLink';
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -12,29 +13,109 @@ const { name } = publicRuntimeConfig.site;
 const Footer = () => {
   return (
     <>
-      <section className="py-3 border-0 border-t-2 border-purple-500">
+      <section className="py-3 border-t-2 border-purple-500">
         <Container>
           <NowPlaying />
         </Container>
       </section>
-      <footer className="py-8 border-0 border-t dark:border-gray-800">
+      <section className="py-8 border-t dark:border-gray-800">
+        <Container>
+          <div className="flex space-x-4">
+            <div className="flex-1">
+              <h2 className="font-semibold text-sm mb-3">Open source</h2>
+              <ul className="list-none list-outside pl-0 mb-0 text-sm space-y-3">
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://github.com/earvinpiamonte"
+                  >
+                    GitHub
+                  </CustomLink>
+                </li>
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://codepen.io/earvinpiamonte"
+                  >
+                    CodePen
+                  </CustomLink>
+                </li>
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://codesandbox.io/u/earvinpiamonte"
+                  >
+                    CodeSandbox
+                  </CustomLink>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <h2 className="font-semibold text-sm mb-3">Hire me</h2>
+              <ul className="list-none list-outside pl-0 mb-0 text-sm space-y-3">
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://www.freelancer.com/u/earvinpiamonte"
+                  >
+                    Freelancer.com
+                  </CustomLink>
+                </li>
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://www.linkedin.com/in/earvinpiamonte"
+                  >
+                    LinkedIn
+                  </CustomLink>
+                </li>
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://ko-fi.com/earvinpiamonte"
+                  >
+                    Ko-fi
+                  </CustomLink>
+                </li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <h2 className="font-semibold text-sm mb-3">Other links</h2>
+              <ul className="list-none list-outside pl-0 mb-0 text-sm space-y-3">
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://drive.google.com/file/d/1ypoi_BirJysxsaaxUxgxR5Zyax4IB-6C/view"
+                  >
+                    Resume
+                  </CustomLink>
+                </li>
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://about.me/earvinpiamonte"
+                  >
+                    about.me
+                  </CustomLink>
+                </li>
+                <li>
+                  <CustomLink
+                    className="text-normal hover:text-purple-500 hover:underline"
+                    href="https://www.buymeacoffee.com/earvinpiamonte"
+                  >
+                    Buy Me a Coffee
+                  </CustomLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Container>
+      </section>
+      <footer className="py-8 border-t dark:border-gray-800">
         <Container>
           <div className="grid grid-cols-12 gap-4 items-center">
-            <div className="col-span-12 md:col-span-6">
-              <div className="md:text-right md:mb-0 mb-4">
-                <a
-                  href="https://www.freelancer.com/u/earvinpiamonte"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="app-link text-gray-800 dark:text-gray-400 text-sm font-normal"
-                >
-                  <ExternalLinkIcon className="w-4 h-4 mr-1 inline-block" />
-                  <span className="align-bottom">Freelancer.com</span>
-                </a>
-              </div>
-            </div>
             <div className="col-span-12 md:col-span-6 md:order-first">
-              <p className="text-sm text-gray-800 dark:text-gray-400 md:mb-0 mb-4">
+              <p className="text-sm text-gray-800 dark:text-gray-400 md:mb-0 mb-0">
                 Designed and built by {name}.
               </p>
             </div>
