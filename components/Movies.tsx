350e064c6d381206ec24213a802ab497db51de48
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-04-03 13:50:14 +0800

    Add movies placeholder

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -10,6 +10,7 @@ const Movies = () => {
   return (
     <>
       <div className="grid grid-cols-3 gap-4">
+        {!data && <MoviesPlaceholder />}
         {data &&
           data.map(({ id, title, thumbnail, date }) => (
             <div key={id}>
@@ -35,6 +36,49 @@ const Movies = () => {
         <CustomLink href="https://developers.themoviedb.org/3">TMDB</CustomLink>
         .
       </p>
+    </>
+  );
+};
+
+const MoviesPlaceholder = () => {
+  return (
+    <>
+      <div className="animate-pulse cursor-wait">
+        <div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-52 md:h-72 mb-2 rounded"></div>
+        </div>
+        <div className="space-y-2 mb-4">
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-20 rounded"></div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-10 rounded"></div>
+        </div>
+      </div>
+      <div className="animate-pulse cursor-wait">
+        <div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-52 md:h-72 mb-2 rounded"></div>
+        </div>
+        <div className="space-y-2 mb-4">
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-16 rounded"></div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-10 rounded"></div>
+        </div>
+      </div>
+      <div className="animate-pulse cursor-wait">
+        <div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-52 md:h-72 mb-2 rounded"></div>
+        </div>
+        <div className="space-y-2 mb-4">
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-20 rounded"></div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-10 rounded"></div>
+        </div>
+      </div>
+      <div className="animate-pulse cursor-wait">
+        <div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-52 md:h-72 mb-2 rounded"></div>
+        </div>
+        <div className="space-y-2 mb-4">
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-16 rounded"></div>
+          <div className="bg-gray-200 dark:bg-gray-800 h-4 w-10 rounded"></div>
+        </div>
+      </div>
     </>
   );
 };
