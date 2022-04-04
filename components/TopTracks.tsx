2c102f396f65f3a15fdda097302b7ed524d6de6f
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-04-04 22:51:05 +0800

    Add bottom margin on top tracks list

--- components/TopTracks.tsx
+++ components/TopTracks.tsx
@@ -22,40 +22,42 @@ const TopTracks = () => {
         </div>
       </div>
       {!data && <TopTracksPlaceholder />}
-      {data &&
-        data.map(({ title, songUrl, artist, albumImageUrl, duration }, i) => (
-          <div
-            key={title}
-            className="flex mb-4 items-center hover:bg-gray-100 py-1 pl-4 rounded group dark:hover:bg-gray-900"
-          >
-            <div className="flex-none w-5 text-right mr-4 relative">
-              <span className="group-hover:opacity-0">{`${i + 1}.`}</span>
-              <a
-                href={songUrl}
-                className="top-0 absolute left-0 right-0"
-                target="_blank"
-                rel="noopener noreferrer"
-                tabIndex={-1}
-              >
-                <PlayIcon className="inline-block w-5 h-5 opacity-0 group-hover:opacity-100 text-purple-500" />
-              </a>
+      <div className="mb-8">
+        {data &&
+          data.map(({ title, songUrl, artist, albumImageUrl, duration }, i) => (
+            <div
+              key={title}
+              className="flex mb-4 items-center hover:bg-gray-100 py-1 pl-4 rounded group dark:hover:bg-gray-900"
+            >
+              <div className="flex-none w-5 text-right mr-4 relative">
+                <span className="group-hover:opacity-0">{`${i + 1}.`}</span>
+                <a
+                  href={songUrl}
+                  className="-top-0.5 absolute left-0 right-0"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  tabIndex={-1}
+                >
+                  <PlayIcon className="inline-block w-5 h-5 opacity-0 group-hover:opacity-100 text-purple-500" />
+                </a>
+              </div>
+              <div className="flex-none w-10 h-10 mr-4 relative">
+                <Image
+                  src={albumImageUrl}
+                  alt="Cover art"
+                  draggable={false}
+                  layout="fill"
+                  className="rounded"
+                />
+              </div>
+              <div className="flex-1 overflow-hidden">
+                <h4 className="font-medium text-base mb-0 truncate">{title}</h4>
+                <p className="mb-0 truncate">{artist}</p>
+              </div>
+              <div className="flex-none pr-4">{duration}</div>
             </div>
-            <div className="flex-none w-10 h-10 mr-4 relative">
-              <Image
-                src={albumImageUrl}
-                alt="Cover art"
-                draggable={false}
-                layout="fill"
-                className="rounded"
-              />
-            </div>
-            <div className="flex-1 overflow-hidden">
-              <h4 className="font-medium text-base mb-0 truncate">{title}</h4>
-              <p className="mb-0 truncate">{artist}</p>
-            </div>
-            <div className="flex-none pr-4">{duration}</div>
-          </div>
-        ))}
+          ))}
+      </div>
       <p className="mb-0 text-gray-600 text-sm">
         Top tracks, currently playing data and images source:{' '}
         <CustomLink href="https://developer.spotify.com/documentation/web-api/">
