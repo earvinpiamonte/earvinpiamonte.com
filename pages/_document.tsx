f42654da6f628ba3f505682ebf6d9f0bb930936c
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-04-26 00:36:13 +0800

    Remove google-site-verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,10 +38,6 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#2d89ef" />
           <meta name="theme-color" content="#000" />
-          <meta
-            name="google-site-verification"
-            content="Yi-IhFWErb7Dp_cIXuV1A49CzR4bybBOHYhJGH2ELnw"
-          />
         </Head>
         <body className="bg-white dark:bg-black dark:text-gray-400">
           <Main />
