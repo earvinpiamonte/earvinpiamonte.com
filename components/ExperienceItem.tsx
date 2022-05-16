80b466edd5abc78d1b7d35bbd3e9aab2ae3ca266
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-05-17 03:12:04 +0800

    Update `ExperienceItem`

--- components/ExperienceItem.tsx
+++ components/ExperienceItem.tsx
@@ -5,23 +5,25 @@ const ExperienceItem = (props) => {
 
   return (
     <>
-      <div className="flex items-center -mt-1">
-        <CheckCircleIcon
-          className={`w-6 h-6 mr-3 flex-none ${
-            index === 0 ? 'text-blue-500' : 'text-blue-500'
-          }`}
-        />
-        <p className="font-medium mb-0 text-blue-500 truncate">{title}</p>
-      </div>
-      <div className="ml-3 -mt-1 pl-6 pb-6 text-gray-600 border-l border-gray-200 dark:border-gray-600">
-        <p className="mb-0">
-          <span className="font-medium">{company}</span> &middot; {location}
-        </p>
-        <p className="mb-0 text-sm">
-          {yearStart}{' '}
-          {yearStart !== yearEnd && <>&mdash; {`${yearEnd ?? 'Present'}`}</>}
-        </p>
-        {tools.length > 0 && <p className="mb-0">{tools.join(', ')}</p>}
+      <div className="flex">
+        <div className="relative before:h-full before:w-[1px] before:bg-gray-200 before:dark:bg-gray-600 before:absolute before:left-3 before:top-1 before:-z-10">
+          <CheckCircleIcon
+            className={`w-6 h-6 flex-none dark:bg-black bg-white ${
+              index === 0 ? 'text-blue-500' : 'text-blue-500'
+            }`}
+          />
+        </div>
+        <div className="px-4 pb-6 text-gray-600">
+          <p className="font-medium mb-0 text-blue-500">{title}</p>
+          <p className="mb-0">
+            <span className="font-medium">{company}</span> &middot; {location}
+          </p>
+          <p className="mb-0 text-sm">
+            {yearStart}{' '}
+            {yearStart !== yearEnd && <>&mdash; {`${yearEnd ?? 'Present'}`}</>}
+          </p>
+          {tools.length > 0 && <p className="mb-0">{tools.join(', ')}</p>}
+        </div>
       </div>
     </>
   );
