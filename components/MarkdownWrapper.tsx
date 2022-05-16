d65d042445b41b4fd8b45ce2c789d783d7c68896
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-05-17 01:03:08 +0800

    Update `prose-code` styles

--- components/MarkdownWrapper.tsx
+++ components/MarkdownWrapper.tsx
@@ -1,6 +1,6 @@
 const MarkdownWrapper = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <div className="prose prose-slate dark:prose-invert prose-headings:text-blue-500 before:prose-code:content-[''] after:prose-code:content-[''] prose-a:text-blue-600 hover:prose-a:text-blue-500 dark:prose-a:text-blue-500 dark:hover:prose-a:text-blue-400 prose-img:rounded-md prose-pre:text-base">
+    <div className="prose prose-slate dark:prose-invert prose-headings:text-blue-500 before:prose-code:content-[''] after:prose-code:content-[''] prose-a:text-blue-600 hover:prose-a:text-blue-500 dark:prose-a:text-blue-500 dark:hover:prose-a:text-blue-400 prose-img:rounded-md prose-pre:text-base prose-code:text-base prose-code:font-normal">
       {children}
     </div>
   );
