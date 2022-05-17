49faab01ed2778cc2ea26810b1636aa458c944df
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-05-18 02:19:20 +0800

    Update 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout';
 import PageTemplate from '@/templates/PageTemplate';
+import Thumbnail from '@/components/Thumbnail';
+import CustomLink from '@/components/CustomLink';
 
 const Custom404 = () => {
   const title = '404 - Page not found';
@@ -10,6 +12,16 @@ const Custom404 = () => {
       <PageTemplate {...{ title }}>
         <p className="text-xl mb-12 text-gray-800 dark:text-gray-400">
           {description}
+        </p>
+        <Thumbnail
+          src="/images/Paranoid Mr Robot GIF.gif"
+          alt="Paranoid Mr Robot GIF"
+        />
+        <p className="text-sm">
+          Image source:{' '}
+          <CustomLink href="https://www.reddit.com/r/reactiongifs/comments/3md7xe/when_i_finally_thought_i_figured_out_what_was/">
+            Reddit
+          </CustomLink>
         </p>
       </PageTemplate>
     </Layout>
