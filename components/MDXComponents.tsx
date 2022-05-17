7eb0a0f673fe832db19e34db955b7ee82d86d468
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-05-18 02:03:00 +0800

    Remove `a` on `MDXComponents`

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image';
 
-import a from '@/components/CustomLink';
 import YouTubeVideo from '@/components/YouTubeVideo';
 import Thumbnail from '@/components/Thumbnail';
 import CodeSandbox from '@/components/CodeSandbox';
 
 const MDXComponents = {
-  a,
   Image,
   Thumbnail,
   YouTubeVideo,
