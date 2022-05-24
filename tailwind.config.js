ef002dac3e218263be67f8a0f80bf3d7dddf91de
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2022-05-25 01:50:46 +0800

    Set `translateX` to `-100%`

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
             transform: 'translateX(0%)',
           },
           '50%': {
-            transform: 'translateX(-80%)',
+            transform: 'translateX(-100%)',
           },
           '95%': {
             transform: 'translateX(0%)',
